53966ffa610b3c71a2d1b89ab1f92a44e84b167e
Ahmad Munif <ahmad.munif02@gmail.com>
2026-01-22 22:49:46 +0700

    Include api/config.php in deployment

---
+++ api/config.php
@@ -0,0 +1,6 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'gjmetony_sood');
+define('DB_USER', 'gjmetony_sood');
+define('DB_PASS', 'm(7;Sp0PA3aD8d');
+define('MIGRATION_KEY', 'RAHASIA');
