dcc5c8f38781a402fefa230194b54a4ac9a5ed66
Ahmad Munif <ahmad.munif02@gmail.com>
2026-01-22 22:59:21 +0700

    Return 204 for check_session to avoid auth loop

--- api/check_session.php
+++ api/check_session.php
@@ -4,7 +4,8 @@ include 'db.php';
 if (isset($_SESSION['user'])) {
     sendResponse($_SESSION['user']);
 } else {
-    http_response_code(401);
+    // Return 204 No Content instead of 401 to prevent global error interceptor from redirecting
+    http_response_code(204);
     exit;
 }
 ?>
