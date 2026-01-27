64e61e32b5bfd6bf8dd25f9cd8a8701e0bbaef29
Ahmad Munif <ahmad.munif02@gmail.com>
2026-01-27 12:04:23 +0700

    fix: add missing closing div tag in LandingPage.tsx

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -276,6 +276,7 @@ const LandingPage: React.FC = () => {
                         <div className="absolute top-0 right-0 -translate-y-1/2 translate-x-1/3 w-64 h-64 bg-white opacity-10 rounded-full"></div>
                         <div className="absolute bottom-0 left-0 translate-y-1/2 -translate-x-1/3 w-64 h-64 bg-white opacity-10 rounded-full"></div>
                     </div>
+                </div>
             </section>
 
             <Footer settings={settings} />
